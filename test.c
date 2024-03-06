607892a95758324cfb52f8996946e41d61b0b6b9
Sheep Yang <yeeyang200011@gmaile.com>
2024-03-06 21:10:55 +0800

    modifiy code

--- test.c
+++ test.c
@@ -1,4 +1,4 @@
-/*Á½¸ö½×³ËºÍÖ®ÉÌ*/
+/*ä¸¤ä¸ªé˜¶ä¹˜å’Œä¹‹å•†*/
 #include <stdio.h>
 
 int f (int n);
@@ -20,42 +20,4 @@ int f(int n)
 		s += sum; 
 	}
 	return (s);
-}
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    double s, r,l;
-    scanf("%lf",&r);
-    circle(&s,&l,r);
-    printf("s = %8.21f\n",s,l);
-}
-void circle(double *p,double*q,double r){
-
-}
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int i;
-    int *p
-     = (int *)malloc(3*sizeof(int));
-    int *q;
-    for(q=p,i=0;i<3;i++,q++)
-    scanf("%d",q);
-    q = p;
-    while(q<q+3)
-    printf("%d\n",*q++);
-    free(p);
-    return 0;
-}
-#include<stdio.h>
-int main()
-{
-int a,b,c;
-a = 152%10;
-b = 152/10%10;
-c = 152/100;
-printf("152 = %d + %d*10 + %d*100",a,b,c);
-return 0;
 }
