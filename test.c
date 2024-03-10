577e9e031c22152677cc7afe7c9bd9267d86e87e
Sheep Yang <yeeyang200011@gmaile.com>
2024-03-10 19:52:16 +0800

    judge year

--- test.c
+++ test.c
@@ -1,23 +1,18 @@
-/*两个阶乘和之商*/
 #include <stdio.h>
-
-int f (int n);
 int main()
 {
-	int x , y;
-	scanf("%d %d",&x, &y);
-	int sum1 = f(x);
-	int sum2 = f(y);
-	double j = sum1*1.0/sum2;
-	printf("%lf",j);
-	return 0;
-}
-int f(int n)
-{
-	int s = 0,sum = 1;
-	for(int i = 1;i <= n;i++){
-		sum *= i;
-		s += sum; 
-	}
-	return (s);
+    int year;
+    scanf("%d",&year);
+    if(year>=100000)
+    {
+        printf("%d-%02d",year/100,year%100);
+    }else {if((year/100)<22)
+    {
+        printf("%d-%02d",20*100+year/100,year%100);
+    }else
+    {
+        printf("%d-%02d",19*100+year/100,year%100);
+    }
+          }
+    return 0;
 }
